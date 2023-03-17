cccba9e7bf11d4d27f12b42cdd0ca2427d140db9
= <=>
2023-03-17 19:12:48 +0530

    addedd new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { Mybutton } from './external_UI_componenet'
+
+function formatName(user){
+  return user.first_name + ' ' + user.last_name;
+}
 
 function App() {
+  
+  const user = { first_name  : "Vishal", last_name : "Thakkar"}
+  const [count, setCount] = useState(0)
+  function handleClick()
+    {
+      setCount(count+1)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Hello {formatName(user)}!!!!</h1>
+      <Mybutton  count={count} onclick={handleClick}/>
+      <h3>Number of Time you pressed the Button: {count}</h3>    
     </div>
   );
 }
---
+++ src/external_UI_componenet.js
@@ -0,0 +1,11 @@
+
+export   function Mybutton({count, onclick}){ 
+    return (
+    <div>
+    <button onClick={onclick}>I'm  a Button</button>
+    
+    </div>
+    
+    );
+  }
+
